964f86c05903ecee4f1d89bdba4b34d4aaf22be2
kidstylex <chilizhiki@gmail.com>
2019-03-20 00:01:06 +0700

    add ship

--- Intothespace.py
+++ Intothespace.py
@@ -3,12 +3,14 @@ import arcade
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 900
 
+
 class GameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height, title='Into the space')
-        arcade.set_background_color(arcade.color.WHEAT)
 
         self.background = None
+        self.ship = arcade.Sprite('img/ship.png')
+        self.ship.set_position(450, 130)
 
     def set_up(self):
         self.background = arcade.load_texture("img/background.jpg")
@@ -17,6 +19,7 @@ class GameWindow(arcade.Window):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.ship.draw()
 
 if __name__ == '__main__':
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
