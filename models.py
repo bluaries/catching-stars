5ded3d50693157907aad73bcaad82bcb4a14410c
kidstylex <chilizhiki@gmail.com>
2019-04-05 14:44:40 +0700

    collect star + update score + test for hit meteorite

--- models.py
+++ models.py
@@ -40,6 +40,7 @@ class World:
         self.height = height
         self.ship = Ship(self, 450, 130)
         self.score = 0
+        self.stage = 1
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
