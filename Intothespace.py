1733ccd04e131dc8f6f7f5d3ee584dd963e0f083
kidstylex <chilizhiki@gmail.com>
2019-05-02 12:56:16 +0700

    test*game over

--- Intothespace.py
+++ Intothespace.py
@@ -1,12 +1,31 @@
+"""
+Artwork from https://www.flaticon.com
+
+"""
+
 import arcade , random
 from models import *
 
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 670
 SCREEN_WIDTH = 900
 METEORITE_COUNT = random.randint(3, 4)
 STAR_COUNT = random.randint(3, 7)
 SPEED_METEO = 6
 SPEED_STAR = 4
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class Meteorite(arcade.Sprite):
     def reset_pos(self):
@@ -20,7 +39,6 @@ class Meteorite(arcade.Sprite):
 
         if self.top < 0:
             self.reset_pos()
-
 
 class Star(arcade.Sprite):
     def reset_pos_star(self):
@@ -41,16 +59,15 @@ class GameWindow(arcade.Window):
         super().__init__(width, height, title='Into the space')
 
         self.background = None
-        self.meteo_sprite_list = None
-        self.star_sprite_list = None
+        self.state = World.STATE_FROZEN
 
         self.world = World(width, height)
         self.ship_sprite = ModelSprite('img/ship.png', model=self.world.ship)
 
-    def add_more_meteotire(self):
+
+    def add_more_meteorite(self):
         for i in range(METEORITE_COUNT):
             meteo = Meteorite('img/meteorite.png')
-
             meteo.center_x = random.randrange(SCREEN_WIDTH)
             meteo.center_y = random.randrange(SCREEN_HEIGHT,
                                               SCREEN_HEIGHT + 300)
@@ -69,25 +86,28 @@ class GameWindow(arcade.Window):
 
     def check_hit_star(self):
         star_hit_list = arcade.check_for_collision_with_list(self.ship_sprite, self.star_sprite_list)
-
         for s in star_hit_list:
             s.reset_pos_star()
             self.world.score += 1
+            # change stage
+            if self.world.score == 25:
+                self.world.stage += 1
 
     def check_hit_meteo(self):
         meteo_hit_list = arcade.check_for_collision_with_list(self.ship_sprite, self.meteo_sprite_list)
         for m in meteo_hit_list:
-            m.reset_pos()
-            self.world.health -= 10
-            if self.world.health == 0:
-                quit()
+            if self.world.health > 0:
+                m.reset_pos()
+                self.world.health -= 10
+                if self.world.health <= 0:
+                    self.world.die()
 
     def set_up(self):
         self.background = arcade.load_texture("img/background.jpg")
         self.meteo_sprite_list = arcade.SpriteList()
         self.star_sprite_list = arcade.SpriteList()
 
-        self.add_more_meteotire()
+        self.add_more_meteorite()
         self.add_more_star()
 
     def on_draw(self):
@@ -102,13 +122,19 @@ class GameWindow(arcade.Window):
                          self.width - 210,
                          self.height - 40,
                          arcade.color.WHITE, 18)
+        self.ship_sprite.draw()
+        self.meteo_sprite_list.draw()
+        self.star_sprite_list.draw()
 
         self.healthbar_sprite = ModelSprite(f'healthbar/h{self.world.health}.png',
                                             model=self.world.healthbar_position)
         self.healthbar_sprite.draw()
-        self.ship_sprite.draw()
-        self.meteo_sprite_list.draw()
-        self.star_sprite_list.draw()
+
+        if self.world.state == 2:
+            arcade.draw_rectangle_filled(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                         SCREEN_WIDTH, SCREEN_HEIGHT, arcade.color.RED_BROWN)
+            arcade.draw_text(f'GAME OVER', 300 , SCREEN_HEIGHT // 2, arcade.color.WHITE, 40)
+
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
 
@@ -118,21 +144,6 @@ class GameWindow(arcade.Window):
         self.star_sprite_list.update()
         self.check_hit_star()
         self.check_hit_meteo()
-
-
-class ModelSprite(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
-
-        super().__init__(*args, **kwargs)
-
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
-
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
 
 
 def main():
--- models.py
+++ models.py
@@ -1,14 +1,15 @@
 import arcade.key
 
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 900
+MOVEMENT_SPEED = 6
+
 DIR_STILL = 0
 DIR_RIGHT = 1
 DIR_LEFT = 2
-
 DIR_OFFSETS = {DIR_STILL: (0, 0),
                DIR_RIGHT: (1, 0),
                DIR_LEFT: (-1, 0)}
-
-MOVEMENT_SPEED = 6
 
 class Model:
     def __init__(self, world, x, y):
@@ -21,7 +22,7 @@ class Ship(Model):
     def __init__(self, world, x, y):
         super().__init__(world, x, y)
 
-        self.direction = DIR_RIGHT
+        self.direction = DIR_STILL
 
     def move(self, direction):
         self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
@@ -34,21 +35,27 @@ class Ship(Model):
         if self.x < 0:
             self.x = 0
 
+
 class Healthbar(Model):
     def __init__(self, world, x, y):
         super().__init__(world, x, y)
 
 
-
 class World:
+    STATE_FROZEN = 1
+    STATE_DEAD = 2
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.ship = Ship(self, 450, 130)
-        self.healthbar_position = Healthbar(self, 130, 550)
+        self.ship = Ship(self, 450, 125)
+
+        self.healthbar_position = Healthbar(self, 130, 620)
         self.score = 0
         self.stage = 1
         self.health = 100
+
+        self.state = World.STATE_FROZEN
 
 
     def on_key_press(self, key, key_modifiers):
@@ -59,5 +66,12 @@ class World:
         if key == arcade.key.RIGHT:
             self.ship.direction = DIR_RIGHT
 
+    def freeze(self):
+        self.state = World.STATE_FROZEN
+
+    def die(self):
+        self.state = World.STATE_DEAD
+
     def update(self, delta):
         self.ship.update(delta)
+
